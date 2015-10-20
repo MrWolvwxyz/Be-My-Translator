9fa219924209562f26db8862aa28f26586def423
Sam Bean <sabean@umich.edu>
2015-10-20 18:09:58 -0400

    Added connections to the viewController

--- BMT Single/ViewController.swift
+++ BMT Single/ViewController.swift
@@ -14,12 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Mark: Properties
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var waitingLabel: UILabel!
+    //Mark: Actions
+    @IBAction func upload(sender: AnyObject) {
     }
 
-
+    @IBAction func imageLanguage(sender: AnyObject) {
+    }
+    @IBAction func nativeLanguage(sender: AnyObject) {
+    }
 }
 
