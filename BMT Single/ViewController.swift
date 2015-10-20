7f7979b1e750b604e6693cc078a5a72c85eb0d5a
Sam Bean <sabean@umich.edu>
2015-10-20 16:44:52 -0400

    Initial Commit

---
+++ BMT Single/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMT Single
+//
+//  Created by samuel bean on 10/20/15.
+//  Copyright (c) 2015 samuel bean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
